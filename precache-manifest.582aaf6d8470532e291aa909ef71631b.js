7c782817ab6cfeac9c3cfed36a08c3cb7d8e14ca
CrioDo <rahul@criodo.com>
2020-10-29 21:09:54 +0530

    Updates

---
+++ precache-manifest.582aaf6d8470532e291aa909ef71631b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27d608fef29f80e832738ed78baaf05",
+    "url": "/portfolio-arkasengupta23/index.html"
+  },
+  {
+    "revision": "61da9f8cebbe6394f69b",
+    "url": "/portfolio-arkasengupta23/static/css/2.57ddf7ad.chunk.css"
+  },
+  {
+    "revision": "3831ef777066deb63cdf",
+    "url": "/portfolio-arkasengupta23/static/css/main.5db1380d.chunk.css"
+  },
+  {
+    "revision": "61da9f8cebbe6394f69b",
+    "url": "/portfolio-arkasengupta23/static/js/2.5218925c.chunk.js"
+  },
+  {
+    "revision": "d466ab9648f013dea081956fc7c904f3",
+    "url": "/portfolio-arkasengupta23/static/js/2.5218925c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3831ef777066deb63cdf",
+    "url": "/portfolio-arkasengupta23/static/js/main.b596a4ac.chunk.js"
+  },
+  {
+    "revision": "19341ea9f7b883207b42",
+    "url": "/portfolio-arkasengupta23/static/js/runtime-main.016adf8a.js"
+  },
+  {
+    "revision": "c9536e0497f939475da3110091e1a10d",
+    "url": "/portfolio-arkasengupta23/static/media/crio-logo-white.c9536e04.svg"
+  },
+  {
+    "revision": "63be71d01edef2dcf3cafba5e52553a4",
+    "url": "/portfolio-arkasengupta23/static/media/crio-logo.63be71d0.svg"
+  }
+]);
